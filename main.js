7bc5ccdcccf68ce8a913f3e727d02d554c3f7d24
Omar Mohamed <99252081+OmarMohammedd@users.noreply.github.com>
2024-07-15 04:26:36 +0300

    update

--- main.js
+++ main.js
@@ -4,20 +4,19 @@ const specialChars = ["%", "*", "/", "-", "+", "="];
 let output = "";
 
 const calculate = (btnValue) => {
-  display.focus();
-  if (btnValue === "=" && output !== "") {
-    output = eval(output.replace("%", "/100"));
-  } else if (btnValue === "AC") {
-    output = "";
-  } else if (btnValue === "DEL") {
-    output = output.toString().slice(0, -1);
-  } else {
-    if (output === "" && specialChars.includes(btnValue)) return;
-    output += btnValue;
-  }
-  display.value = output;
+    if (btnValue === "=" && output !== "") {
+        output = eval(output.replace("%", "/100"));
+    } else if (btnValue === "AC") {
+        output = "";
+    } else if (btnValue === "DEL") {
+        output = output.toString().slice(0, -1);
+    } else {
+        if (output === "" && specialChars.includes(btnValue)) return;
+        output += btnValue;
+    }
+    display.value = output;
 };
 
 buttons.forEach((button) => {
-  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+    button.addEventListener("click", (e) => calculate(e.target.dataset.value));
 });
